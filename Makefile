b9b742f68fd9cb05670dc52075e3e65eefb94381
Zülfiye Kayadibi <zkayadib@k1m06s03.42istanbul.com.tr>
2025-10-02 21:22:42 +0300

    feat: external files added in the external folder.

--- Makefile
+++ Makefile
@@ -3,16 +3,17 @@
 #                                                         :::      ::::::::    #
 #    Makefile                                           :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: ysumeral <ysumeral@student.42.fr>          +#+  +:+       +#+         #
+#    By: zkayadib <zkayadib@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/09/28 20:23:36 by ysumeral          #+#    #+#              #
-#    Updated: 2025/10/02 18:50:30 by ysumeral         ###   ########.fr        #
+#    Updated: 2025/10/02 21:01:01 by zkayadib         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 CC = cc
 CFLAGS = -Wall -Wextra -Werror -I${INC_DIR}
 SRC_DIR = ./src
+GNL_DIR = ./external
 MLX_DIR = ./mlx
 INC_DIR = ./include
 MLX = -L$(MLX_DIR) -lmlx -lX11 -lXext
@@ -24,17 +25,20 @@ SRC =	$(SRC_DIR)/main.c \
 		$(SRC_DIR)/parse.c \
 		$(SRC_DIR)/init.c \
 		$(SRC_DIR)/game.c
+GNL =	$(GNL_DIR)/get_next_line.c \
+		$(GNL_DIR)/get_next_line_utils.c
 OBJ = $(SRC:.c=.o)
+OBJ_GNL = $(GNL:.c=.o)
 NAME = cub3D
 
 all: $(NAME)
 
-$(NAME): $(OBJ)
+$(NAME): $(OBJ) $(OBJ_GNL)
 	make -C $(MLX_DIR)
-	$(CC) $(CFLAGS) -o $(NAME) $(OBJ) ${PRINTF_LIB} $(MLX)
+	$(CC) $(CFLAGS) -o $(NAME) $(OBJ) $(OBJ_GNL) ${PRINTF_LIB} $(MLX)
 
 clean:
-	rm -f $(OBJ)
+	rm -f $(OBJ) $(OBJ_GNL)
 
 fclean: clean
 	make -C $(MLX_DIR) clean
---
+++ external/get_next_line.c
@@ -0,0 +1,126 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zkayadib <zkayadib@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/23 21:30:33 by zkayadib          #+#    #+#             */
+/*   Updated: 2024/12/25 20:36:59 by zkayadib         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*findnewline(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (!str)
+		return (NULL);
+	while (str[i])
+	{
+		if (str[i] == '\n')
+			return (str + i);
+		i++;
+	}
+	return (NULL);
+}
+
+char	*beforenewline(char *str)
+{
+	int		i;
+	char	*line;
+	int		has_nl;
+
+	has_nl = 0;
+	i = 0;
+	if (!str || str[0] == '\0')
+		return (NULL);
+	while (str[i] && str[i] != '\n')
+		i++;
+	if (str[i] == '\n')
+	{
+		has_nl = 1;
+		i++;
+	}
+	line = ft_calloc(sizeof(char), i + 1);
+	if (!line)
+		return (NULL);
+	i = -1;
+	while (str && str[++i] && str[i] != '\n')
+		line[i] = str[i];
+	if (has_nl)
+		line[i] = '\n';
+	return (line);
+}
+
+char	*after_new_line(char *s)
+{
+	int		i;
+	int		j;
+	char	*ret;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	while (s[i] && s[i] != '\n')
+		i++;
+	if (s[i] == '\n')
+		i++;
+	ret = ft_calloc(sizeof(char), ft_strlen(s) - i + 1);
+	if (!ret)
+		return (NULL);
+	j = 0;
+	while (s[i])
+	{
+		ret[j++] = s[i++];
+	}
+	free(s);
+	return (ret);
+}
+
+char	*read_func(int *rd, int fd, char *after)
+{
+	char	*buffer;
+
+	buffer = ft_calloc(sizeof(char), BUFFER_SIZE + 1);
+	if (!buffer)
+		return (NULL);
+	*rd = read(fd, buffer, BUFFER_SIZE);
+	if (*rd == -1)
+	{
+		free(after);
+		free(buffer);
+		return (NULL);
+	}
+	after = ft_strjoin(after, buffer);
+	free(buffer);
+	return (after);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*result;
+	int			rd;
+	static char	*after;
+
+	rd = 1;
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	while (rd > 0 && !findnewline(after))
+	{
+		after = read_func(&rd, fd, after);
+		if (!after)
+			return (NULL);
+	}
+	result = beforenewline(after);
+	after = after_new_line(after);
+	if (after && after[0] == 0)
+	{
+		free(after);
+		after = NULL;
+	}
+	return (result);
+}
